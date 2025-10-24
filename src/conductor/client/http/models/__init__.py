a5cde131398015ff4bb3d0e38dda2b1a9ed9ed8a
Xavier Canal Masjuan <xavier.canal.ext@cloudfactory.com>
2025-10-24 10:34:48 +0200

    Add export WebHookConfig outside the module
    Co-authored-by: Xavier Canal Masjuan <xeviknal@gmail.com>

--- src/conductor/client/http/models/__init__.py
+++ src/conductor/client/http/models/__init__.py
@@ -257,6 +257,7 @@ from conductor.client.http.models.search_result_workflow_summary import SearchRe
 from conductor.client.http.models.start_workflow import StartWorkflow
 from conductor.shared.http.enums.idempotency_strategy import IdempotencyStrategy
 from conductor.client.http.models.task_result_status import TaskResultStatus
+from conductor.client.http.models.webhook_config import WebhookConfig
 
 __all__ = [  # noqa: RUF022
     "Action",
@@ -403,4 +404,5 @@ __all__ = [  # noqa: RUF022
     "StateChangeEventType",
     "StateChangeConfig",
     "TaskResultStatus",
+    "WebhookConfig",
 ]
